e1c9ddf7d57cfd500dead0ee3613d8ad571aca33
AlexNdungu <alexmeta517@gmail.com>
2023-08-04 14:38:32 +0300

    Email Stripper

--- generate.py
+++ generate.py
@@ -8,3 +8,27 @@ class UserGen():
         
         self. email = email
         self.max_num = max_num
+
+
+    def StripMail(self):
+
+        '''
+        This method Strips an email
+        Example:
+        1. Email = dev@gmail.com 
+            returns: dev
+        2. Email = testmail@yahoo.com
+            returns: testmail    
+        '''
+
+        mail_extract = self.email.split('@')[0]
+
+        return mail_extract
+
+    def GenNumWithMax(self):
+
+        '''
+        This method generates a random number considering the maximum number passed to it
+        '''
+
+        
