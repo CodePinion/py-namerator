df363123293925563ab4fb4fbacb49d8be1a27f5
AlexNdungu <alexmeta517@gmail.com>
2023-08-04 16:44:00 +0300

    Test Email Strippert

--- py_tests/test_mail_strip.py
+++ py_tests/test_mail_strip.py
@@ -1,4 +1,8 @@
-from generate import UserGen
+import sys
+
+sys.path.insert(1, './/Code')
+
+import generate
 
 valid_email_formats = [
     'example@example.com',
@@ -18,7 +22,7 @@ def test_smail_strip():
 
     for email in valid_email_formats:
 
-        user_gen_class = UserGen(email,0)
+        user_gen_class = generate.UserGen(email,0)
 
         print(user_gen_class.StripMail())
 
@@ -27,3 +31,5 @@ def test_smail_strip():
 if __name__ == "__main__":
     test_smail_strip()
     print("Everything passed")
+
+#random_numbers = [22, 23, 80, 2, 44, 5, 49, 46, 50, 21]
---
+++ py_tests/test_num_gen.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.path.insert(1, './/Code')
+
+import generate
+
+
+
+random_numbers = [22, 23, 80, 2, 44, 5, 49, 46, 50, 21]
